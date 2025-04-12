0ab0a8ab6753e1f15270cef56ec049fc130fb895
Juan Carlos <71660350+akdgit@users.noreply.github.com>
2025-04-12 17:25:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ app.use(session({
 
 //setting
 //app.set("port", 4000);
-const PORT = 4000;
+/*const PORT = 4000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+});*/
 //middleware
 
 app.use(cors({
