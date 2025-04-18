4c4ce34c04985b0e9e9811969ec773d4255199ce
Juan Camacaro <juanc.camacaro@gmail.com>
2025-04-18 10:37:04 -0500

    Change config.js

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ config();
 //railway servr backend
 export default {
     database: {
-        host: process.env.MYSQL_HOST || 'mysql.railway.internal',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || 'GsxfHYccOQErmPwUiVAJzXAxiutqzemS',
-        database: process.env.MYSQL_DATABASE || 'railway'
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
     }
     
 };
