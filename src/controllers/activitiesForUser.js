bfd6dfc651eeedc5733a0226861b1121dc339aeb
Juan Camacaro <juanc.camacaro@gmail.com>
2025-04-27 16:09:53 -0500

    Change on actvities users response array

--- src/controllers/activitiesForUser.js
+++ src/controllers/activitiesForUser.js
@@ -10,7 +10,7 @@ const getActivitiesUsers = async(req, res) => {
         const result = await connection.query("SELECT tk.id, tk.description, done, tk.start_date, tk.end_date, tk.user_id FROM tasks tk, users us WHERE us.id = ? AND tk.user_id = us.id ", [user_id]  );
         console.log(result);
         console.log("Resultado de DB:", result);
-        res.json(result);
+        res.json(result[0]);
     } catch (error) {
         res.status(500).send(error.message);
     }
